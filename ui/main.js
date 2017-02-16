299c0364b7f2d9303da8094222850167537a84e1
itsprankur <me.prankurchauchan@gmail.com>
2017-02-16 19:41:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ function moveRight ()
 
 img.onclick = function ()
 {
-    var interval = setInterval(moveRight , 100);
+    var interval = setInterval(moveRight , 10);
     img.style.marginLeft = '100px';
     
 };
