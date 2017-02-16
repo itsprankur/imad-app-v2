75a0029c7a3335bcaee7d672e37267797d4bfbbb
itsprankur <me.prankurchauchan@gmail.com>
2017-02-16 14:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'This is a web page made by Prankur Chauhan';
+
+//move image
+
+var img = document.getElementById('madi');
+
+img.onclick = func(){
+    img.style.marginLeft = '100px';
+    
+}
